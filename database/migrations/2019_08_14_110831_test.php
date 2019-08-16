887944d8032c6ce79c3f06bc9b8e06a1e3a77ade
darksl <dasundevinda25@gmai.com>
2019-08-16 21:57:09 +0530

    First Commit

---
+++ database/migrations/2019_08_14_110831_test.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Test extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('test', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fname',255);
+            $table->string('lname',255);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
